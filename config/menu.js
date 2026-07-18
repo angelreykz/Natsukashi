cb71dbc773d39fecb58067721b1f5fc78b2ae282
reykz <nasoki.0802@gmail.com>
2026-07-18 13:09:35 -0300

    Update menu.js

--- config/menu.js
+++ config/menu.js
@@ -1,1 +1,12 @@
+module.exports = {
+  titulo: "🤖 Natsukashii",
 
+  descricao: "Bot para WhatsApp",
+
+  categorias: [
+    "👤 Dono",
+    "🛡 Administração",
+    "🎮 Diversão",
+    "⚙ Utilidades"
+  ]
+};
