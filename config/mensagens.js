10953075cfffba878665b722b4b0c41f4ea22175
reykz <nasoki.0802@gmail.com>
2026-07-18 13:09:20 -0300

    Update mensagens.js

--- config/mensagens.js
+++ config/mensagens.js
@@ -1,1 +1,11 @@
+module.exports = {
+  sucesso: "✅ Operação realizada com sucesso!",
 
+  erro: "❌ Ocorreu um erro.",
+
+  semPermissao: "❌ Você não tem permissão para usar este comando.",
+
+  somenteGrupo: "❌ Este comando só funciona em grupos.",
+
+  somentePrivado: "❌ Este comando só funciona no privado."
+};
